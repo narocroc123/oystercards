1637f99d75508c08e5fa9462f1a7499427193944
Fergus Corcoran <fergus.corcoran@gmail.com>
2019-12-16 15:32:24 +0000

    Challenge 4: Adding Balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,15 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  it 'responds to instance of oystercard' do
+    oystercard = Oystercard.new
+    expect(oystercard).to be_kind_of Oystercard
+  end
+
+  describe '#balance' do
+    it 'responds to balance method' do
+      oystercard = Oystercard.new
+      expect(oystercard).to respond_to(:balance)
+    end
+  end
 end
